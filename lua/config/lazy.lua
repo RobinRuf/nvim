fba3cc79161111b04aae7fc793422861500f381e
Robin <robin@macbookair.home>
2023-11-16 04:43:28 +0100

    feat(lazyvim): cloned lazyvim and configured for my needs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,19 +1,42 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "solarized-osaka",
+        news = {
+          lazyvim = true,
+          neovim = true,
+        },
+      },
+    },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    --     { import = "lazyvim.plugins.extras.coding.copilot" }, GitHub Copilot Membership needed
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -26,7 +49,10 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  -- path for local scripts or modules
+  dev = {
+    path = "~/.ghq/github.com/",
+  },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
@@ -35,7 +61,8 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
+        "rplugin",
         "tarPlugin",
         "tohtml",
         "tutor",
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,64 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Basic Configs
+vim.g.mapleader = " " -- sets SPACE to the leader for special commands
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true -- enables line numbers
+
+vim.opt.title = true -- shows the file name in the terminal title
+vim.opt.autoindent = true
+vim.opt.smartindent = true -- extended indent
+vim.opt.hlsearch = true -- highlights search results
+vim.opt.backup = false -- disables backups
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2 -- always shows the statusbar
+vim.opt.expandtab = true -- change tabs to spaces
+vim.opt.scrolloff = 10 -- keeps the cursor on 10th line from above while scolling up
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" } -- do not take any backups of these folders
+vim.opt.inccommand = "split" -- shows preview of incremental rename
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smarttab = true
+vim.opt.breakindent = true -- keeps the indent, even line will be switched
+vim.opt.shiftwidth = 2 -- defines 2 spaces as indent
+vim.opt.tabstop = 2 -- defines 2 spaces as indent with tabs
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({
+  "*/node_modules/*",
+  "*/build/*",
+  "*/target/*",
+  "*/venv/*",
+  "*/.venv/*",
+  "*/__pycache__/*",
+  "*/.git/*",
+  "*.a",
+  "*.o",
+}) -- defines, which folders should be ignored while searching
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitkeep = "cursor" -- keeps the cursor at the same position while splitting windows
+vim.opt.mouse = "" -- disables mouse
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Add asterisks in block comments for longer comments
+vim.opt.formatoptions:append({ "r" })
+
+-- sets the fileformat for .astro files to astro and for Podfile files (like in iOS Development) to Ruby files
+vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
+
+-- checks if the used nvim version is 0.8 or above and if so, it sets cmdheight to 0 so the cmd line is just visible if used and not everytime
+if vim.fn.has("nvim-0.8") == 1 then
+  vim.opt.cmdheight = 0
+end
